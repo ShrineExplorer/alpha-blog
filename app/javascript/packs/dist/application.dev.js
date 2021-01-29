5d4ad2ca71e03747425c3684cdd5982921cc41d5
TheLostBoi <rock98835@gmail.com>
2021-01-29 21:52:48 +0530

    Extract redundancy and prepare for production

--- app/javascript/packs/dist/application.dev.js
+++ app/javascript/packs/dist/application.dev.js
@@ -2,7 +2,7 @@
 
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
-require("../stylesheets/style");
+require("assets/stylesheets/style");
 
 var _ujs = _interopRequireDefault(require("@rails/ujs"));
 
