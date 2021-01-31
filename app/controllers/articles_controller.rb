007713ebe83178b624f5a74e16f875bc67d5e23b
TheLostBoi <rock98835@gmail.com>
2021-01-31 05:42:59 +0530

    Add association between user and article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,9 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     def create 
+        debugger
         @article = Article.new(article_params)
+        @article.user = User.first
         if @article.save
             flash[:succsess]="Article was successfully created"
             redirect_to article_path(@article)
